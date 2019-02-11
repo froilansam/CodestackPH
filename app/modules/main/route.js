a3456cfdc50b39556cfed7391729859a47d7f6d7
Froilan Malibiran <froilansam@gmail.com>
2019-02-11 18:23:29 +0800

    As of feb 11

--- app/modules/main/route.js
+++ app/modules/main/route.js
@@ -3,13 +3,134 @@ var router = express.Router();
 var logInRouter = express.Router();
 var authMiddleware = require('../auth/middleware');
 var db = require('../../lib/database')();
+//Firebase
+var firebase = require('firebase');
+var config = {
+  apiKey: 'AIzaSyDnf5a8rXG7T4YkYwFSuB9eWpwt6xKmAN0',
+  authDomain: 'codestack-ph.firebaseapp.com',
+  databaseURL: 'codestack-ph.firebaseio.com',
+  projectId: 'codestack-ph',
+  storageBucket: 'codestack-ph.appspot.com',
+  messagingSenderId: '1689996503'
+}
+firebase.initializeApp(config);
+var database = firebase.firestore();
+let aboutus = database.collection('aboutus');
+let contactus = database.collection('contactus');
+let testimonial = database.collection('testimonial');
+var socket = require('socket.io-client')(`http://localhost:3000`)
+aboutus.onSnapshot(function(){ //ON SNAPSHOT FUNCTION IS A LISTENER TO CHANGES IN THE DATABASE
+    aboutus.get().then(collection => {
+        console.log('----Collection: About Us----')
+        collection.forEach(doc => {
+            console.log(doc.data());
+            socket.emit('aboutusBackend', doc.data(), function(){
+                console.log('Data Sent to Index.')
+            });
+        });
+
+    });
+});
+contactus.onSnapshot(function(){ //ON SNAPSHOT FUNCTION IS A LISTENER TO CHANGES IN THE DATABASE
+    contactus.get().then(collection => {
+        console.log('----Collection: Contact Us ----')
+        collection.forEach(doc => {
+            console.log(doc.data());
+            socket.emit('contactusBackend', doc.data(), function(){
+                console.log('Data Sent to Index.')
+            });
+        });
+
+    });
+});
+testimonial.onSnapshot(function(){ //ON SNAPSHOT FUNCTION IS A LISTENER TO CHANGES IN THE DATABASE
+    testimonial.get().then(collection => {
+        console.log('----Collection: Testimonial ----')
+        collection.forEach(doc => {
+            console.log(doc.data());
+            socket.emit('testimonialBackend', doc.data(), function(){
+                console.log('Data Sent to Index.')
+            });
+        });
+
+    });
+});
 
 
 logInRouter.use(authMiddleware.noAuthed);
-logInRouter.get('/', (req, res) => {
-    console.log('Log In Router');
-    res.render('main/views/login', req.query);
+
+router.get('/', (req, res) => {
+    console.log('--Index Router--');
+    socket = require('socket.io-client')(`http://${req.hostname}:${req.port}`)
+    console.log(`http://${req.hostname}:${req.port}`)
+    // Getting all the data
+    function getAboutUs(){
+        return new Promise((resolve, reject)=>{
+            let numberProcessed = 0;
+            aboutus.get().then(collection => {
+                let collectionLength = collection.size;
+                collection.forEach(doc => {
+                    numberProcessed++;
+                    console.log(collectionLength)
+                    if(numberProcessed === collectionLength){
+                        resolve(doc.data());
+                    }
+                });
+            });
+        });
+    }
+
+    function getContactUs(){
+        return new Promise((resolve, reject)=>{
+            let numberProcessed = 0;
+            contactus.get().then(collection => {
+                let collectionLength = collection.size;
+                collection.forEach(doc => {
+                    numberProcessed++;
+                    console.log(collectionLength)
+                    if(numberProcessed === collectionLength){
+                        resolve(doc.data());
+                    }
+                });
+            });
+        });
+    }
+    function getTestimonial(){
+        return new Promise((resolve, reject)=>{
+            let numberProcessed = 0;
+            let testimonials = []
+            testimonial.get().then(collection => {
+                let collectionLength = collection.size;
+                collection.forEach(doc => {
+                    numberProcessed++;
+                    console.log(collectionLength)
+                    testimonials.push(doc.data())
+                    if(numberProcessed === collectionLength){
+                        resolve(testimonials);
+                    }
+                });
+            });
+        });
+    }
+    // Getting all the data
+    
+
+    getAboutUs().then(docData => {
+        let docAbout = docData;
+        getContactUs().then(docData => {
+            let docContact = docData;
+            getTestimonial().then(docData => {
+                let docTestimonial = docData
+                console.log(docTestimonial)
+                res.render('main/views/index', {hostname: req.hostname, port: req.port, docAbout: docAbout, docContact: docContact, docTestimonial: docTestimonial});
+            });            
+        });        
+    })
 })
+
+
+
+
 .post('/', (req, res) => {
 
 
--- index.js
+++ index.js
@@ -7,9 +7,40 @@ var express = require('express');
 // Creating an ExpressJS App
 var app = express();
 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.setMaxListeners(0);
+io.setMaxListeners(0);
+
+
+app.use(function(req,res,next){
+    req.io = io;
+    next();
+    req.port = app.get('port');
+});
+
+//Socket
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('aboutusBackend', function(aboutusData){
+        io.emit('aboutusClient', aboutusData);
+    })
+    socket.on('contactusBackend', function(contactusData){
+        io.emit('contactusClient', contactusData);
+    })
+    socket.on('testimonialBackend', function(testimonialData){
+        io.emit('testimonialClient', testimonialData);
+    })
+});
+
 // We now pass the app instance to a custom module 'app' for bootstrapping\\
 require('./app')(app);
 
-app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
     console.log(`Server is listening to the port ${app.get('port')}`);
 })
